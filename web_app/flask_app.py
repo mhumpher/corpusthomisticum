a0a4994e48e0e69c1427247ad5443ee119a52d7e
mhumpher <mhumpher@gmail.com>
2020-10-23 09:13:41 -0500

    Update flask_app.py

--- web_app/flask_app.py
+++ web_app/flask_app.py
@@ -32,14 +32,16 @@ def my_form_post():
     text = request.form['text'].encode('unicode-escape').decode()
     #processed_text = text.upper()
 
-    df = pd.read_sql('select `Subheader 1`, `Section Index`, site from corpusthomisticum where Content REGEXP "' + text + '"', con = db.engine)
+    df = pd.read_sql('select `Subheader 1`, `Section Index`, site, Content from corpusthomisticum where Content REGEXP "' + text + '"', con = db.engine)
     df = df.rename(columns = {'Subheader 1':'Work', 'Section Index':'Passage', 'site':'Site'})
     df['Site'] = df['Site'] + "#" + df['Passage'].str.extract(r'\[([0-9]+)\]')
     df['Passage'] = '<a href="' + df['Site'] + '" target="_blank">' + df['Passage'] + '</a>'
+    df['Selection'] = "..." + df['Content'].str.extract(r"(.{0,50}" + text + r".{0,50})", flags = re.IGNORECASE) + "..."
+    df = df.drop(['Content', 'Site'],axis = 1)
     #lt = df.values.tolist()
     #lt = list(map(str, lt))
     #print(lt)
     #return 1
     size = df['Passage'].size
     searchTerm = text
-    return render_template('CorpThomOutput.html', tables=[df.to_html(classes='data', index = False, escape = False)], titles=df.columns.values, searchTerm = searchTerm, size = size)
+    return render_template('CorpThomOutput.html', tables=[df.to_html(classes='data', index = False, escape = False, justify = 'center')], titles=df.columns.values, searchTerm = searchTerm, size = size)
